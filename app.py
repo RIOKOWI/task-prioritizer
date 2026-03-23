cd9c924934a6ef3fbb2f86f0c9fd61fde91a1785
RIOKOWI <achyarrio2004@gmail.com>
2026-03-23 15:09:40 +0700

    feat: initialize hebbian prioritizer model and basic streamlit layout

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import numpy as np
 import pandas as pd
 
 class HebbianPrioritizer:
+    def __init__(self, tasks, time_slots):
+        self.tasks = tasks            
+        self.time_slots = time_slots
